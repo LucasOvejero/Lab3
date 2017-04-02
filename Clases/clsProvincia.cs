d7b6d8561699b932e1af7d6075908bf45fd163b2
Lucas <Lucas>
2017-04-02 17:02:38 -0300

    prueba que no se borra

---
+++ Clases/clsProvincia.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+namespace Clases
+{
+    public class clsProvincia
+    {
+        int idProvincia;
+        string nombreProvincia;
+        SqlCommand comando;
+        SqlDataAdapter adaptador= new SqlDataAdapter();
+        public int IdProvincia{
+            get { return idProvincia; }
+            set { idProvincia = value; }
+        }
+        public string NombreProvincia {
+            get { return nombreProvincia; }
+            set { nombreProvincia = value; }
+        }
+
+        public DataTable seleccionarProvincias() {
+            DataTable prov = new DataTable("Provincias");
+            comando = new SqlCommand("Select * from provincia");
+            try
+            {
+                comando.Connection = clsConexion.getCon();
+                adaptador.SelectCommand = comando;
+                adaptador.Fill(prov);
+            }
+            catch (SqlException e) { }
+            finally { clsConexion.closeCon(); }
+            return prov;      
+        }
+    }
+}
